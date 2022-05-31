56d06e9a7a4e0623ef564422bb5a7a46227b10a8
Frank <760776821@qq.com>
2022-05-31 17:01:34 +0800

    调整了目录的初始结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+// 项目入口文件
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// 加载全局样式文件
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
@@ -8,3 +12,9 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+/*
+
+components里面可以叫做公共组件，views里面可以叫做视图组件
+
+*/
